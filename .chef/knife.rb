72be7d3b63748686e53a134554470e5292c58603
kbeckman <kbeckman.c4sc@gmail.com>
2016-07-15 16:58:52 -0500

    knife.rb changes for chef-zero

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,8 +1,9 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
-node_path        "nodes"
-role_path        "roles"
-environment_path "environments"
-data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
+cookbook_path             ['cookbooks']
+node_path                 'nodes'
+role_path                 'roles'
+environment_path          'environments'
+data_bag_path             'data_bags'
+encrypted_data_bag_secret 'data_bag_key'
 
-knife[:berkshelf_path] = "cookbooks"
+chef_zero.enabled true
+local_mode        true
