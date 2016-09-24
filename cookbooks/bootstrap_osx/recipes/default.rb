305914906e28604afae779496a6d5ed105409c61
kbeckman <kbeckman.c4sc@gmail.com>
2016-09-23 23:05:40 -0500

    removing default recipe (homebrew no longer needs ownership of /usr/local)

--- cookbooks/bootstrap_osx/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-osx_user  = node['bootstrap_osx']['osx_user']
-
-# Requirement for "homebrew" recipe...
-# https://github.com/chef-cookbooks/homebrew
-directory '/usr/local' do
-  owner   osx_user
-  group   'admin'
-  mode    '0755'
-end
