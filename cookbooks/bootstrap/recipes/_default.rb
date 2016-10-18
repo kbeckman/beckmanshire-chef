c4809fd7555d24fbc0e7aebb2a5f24db58b76be1
kbeckman <kbeckman.c4sc@gmail.com>
2016-10-17 20:35:51 -0500

    adding ::_homesick default attributes

--- cookbooks/bootstrap/recipes/_default.rb
+++ cookbooks/bootstrap/recipes/_default.rb
@@ -1,3 +1,11 @@
 user_msg = "node['#{cookbook_name}']['user'] must be set!"
 raise user_msg if node[cookbook_name]['user'].nil?
 raise user_msg if node[cookbook_name]['user'].length == 0
+
+castle_msg = "node['#{cookbook_name}']['homesick']['castle_name'] must be set!"
+raise castle_msg if node[cookbook_name]['homesick']['castle_name'].nil?
+raise castle_msg if node[cookbook_name]['homesick']['castle_name'].length == 0
+
+repo_msg = "node['#{cookbook_name}']['homesick']['github_repo'] must be set!"
+raise repo_msg if node[cookbook_name]['homesick']['github_repo'].nil?
+raise repo_msg if node[cookbook_name]['homesick']['github_repo'].length == 0
--- cookbooks/bootstrap/spec/unit/recipes/_default_spec.rb
+++ cookbooks/bootstrap/spec/unit/recipes/_default_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 RSpec.describe "#{COOKBOOK_NAME}::_default" do
+  let(:valid_user) { 'rflair' }
+  let(:valid_castle_name) { 'limousine_ridin' }
+  let(:valid_github_repo) { 'git://github.com/rflair/castle' }
   let(:chef_run) do
     ChefSpec::SoloRunner.new do |node|
       node.normal[COOKBOOK_NAME]['user'] = 'jduggan'
@@ -14,7 +17,9 @@ RSpec.describe "#{COOKBOOK_NAME}::_default" do
       it "raises an exeption when node['#{COOKBOOK_NAME}']['user'] is nil" do
         chef_run = lambda do
           ChefSpec::SoloRunner.new do |node|
-            node.normal[COOKBOOK_NAME]['user'] = nil
+            node.normal[COOKBOOK_NAME]['user']                    = nil
+            node.normal[COOKBOOK_NAME]['homesick']['castle_name'] = valid_castle_name
+            node.normal[COOKBOOK_NAME]['homesick']['github_repo'] = valid_github_repo
           end.converge(described_recipe)
         end
 
@@ -24,7 +29,65 @@ RSpec.describe "#{COOKBOOK_NAME}::_default" do
       it "raises an exeption when node['#{COOKBOOK_NAME}']['user'] is empty" do
         chef_run = lambda do
           ChefSpec::SoloRunner.new do |node|
-            node.normal[COOKBOOK_NAME]['user'] = ''
+            node.normal[COOKBOOK_NAME]['user']                    = ''
+            node.normal[COOKBOOK_NAME]['homesick']['castle_name'] = valid_castle_name
+            node.normal[COOKBOOK_NAME]['homesick']['github_repo'] = valid_github_repo
+          end.converge(described_recipe)
+        end
+
+        expect { chef_run.call }.to raise_error(error_msg)
+      end
+    end
+
+    context "node['#{COOKBOOK_NAME}']['homesick']['castle_name]'" do
+      let(:error_msg) { "node['#{COOKBOOK_NAME}']['homesick']['castle_name'] must be set!" }
+
+      it "raises an exeption when node['#{COOKBOOK_NAME}']['homesick']['castle_name'] is nil" do
+        chef_run = lambda do
+          ChefSpec::SoloRunner.new do |node|
+            node.normal[COOKBOOK_NAME]['user']                    = valid_user
+            node.normal[COOKBOOK_NAME]['homesick']['castle_name'] = nil
+            node.normal[COOKBOOK_NAME]['homesick']['github_repo'] = valid_github_repo
+          end.converge(described_recipe)
+        end
+
+        expect { chef_run.call }.to raise_error(error_msg)
+      end
+
+      it "raises an exeption when node['#{COOKBOOK_NAME}']['homesick']['castle_name'] is empty" do
+        chef_run = lambda do
+          ChefSpec::SoloRunner.new do |node|
+            node.normal[COOKBOOK_NAME]['user']                    = valid_user
+            node.normal[COOKBOOK_NAME]['homesick']['castle_name'] = ''
+            node.normal[COOKBOOK_NAME]['homesick']['github_repo'] = valid_github_repo
+          end.converge(described_recipe)
+        end
+
+        expect { chef_run.call }.to raise_error(error_msg)
+      end
+    end
+
+    context "node['#{COOKBOOK_NAME}']['homesick']['github_repo]'" do
+      let(:error_msg) { "node['#{COOKBOOK_NAME}']['homesick']['github_repo'] must be set!" }
+
+      it "raises an exeption when node['#{COOKBOOK_NAME}']['homesick']['github_repo'] is nil" do
+        chef_run = lambda do
+          ChefSpec::SoloRunner.new do |node|
+            node.normal[COOKBOOK_NAME]['user']                    = valid_user
+            node.normal[COOKBOOK_NAME]['homesick']['castle_name'] = valid_castle_name
+            node.normal[COOKBOOK_NAME]['homesick']['github_repo'] = nil
+          end.converge(described_recipe)
+        end
+
+        expect { chef_run.call }.to raise_error(error_msg)
+      end
+
+      it "raises an exeption when node['#{COOKBOOK_NAME}']['homesick']['github_repo'] is empty" do
+        chef_run = lambda do
+          ChefSpec::SoloRunner.new do |node|
+            node.normal[COOKBOOK_NAME]['user']                    = valid_user
+            node.normal[COOKBOOK_NAME]['homesick']['castle_name'] = valid_castle_name
+            node.normal[COOKBOOK_NAME]['homesick']['github_repo'] = ''
           end.converge(described_recipe)
         end
 
