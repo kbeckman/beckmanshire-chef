8789d6e1a69f80ed05cab7fd5728a4243df16076
kbeckman <kbeckman@redfournine.com>
2017-11-21 12:59:38 -0600

    ::_homebrew changes for Chef 13; updated 'homebrew' cookbook dependency

--- cookbooks/bootstrap/spec/spec_helper.rb
+++ cookbooks/bootstrap/spec/spec_helper.rb
@@ -3,3 +3,7 @@ require 'chefspec/berkshelf'
 require 'pry'
 
 COOKBOOK_NAME = 'bootstrap'.freeze
+
+# NOTE: ChefDK 2.3.4 does not officially include support for mac_os_x/10.13 (High Sierra)...
+# TODO: Keep an eye on this and change it when the appropriate updates have been made.
+CHEF_SPEC_OPTS = { platform: 'mac_os_x', version: '10.12' }.freeze
